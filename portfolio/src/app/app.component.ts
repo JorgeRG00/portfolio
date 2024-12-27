18f39a858575a271561443ce459281b76b6b0d4c
Jorge Roldan <jorgeroldan00.jr@gmail.com>
2024-12-27 11:01:18 +0100

    Terminados componentes

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { AboutComponent } from "./components/about/about.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeaderComponent, ProjectsComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
